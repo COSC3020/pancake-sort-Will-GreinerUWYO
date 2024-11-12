bd85d9481f1e3e0308869e5ca04a0332ddd68aa6
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-12 09:54:38 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,32 @@
 function flip(array, n) {
+    var temp  = 0;
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        temp = array[i];
+        array[i] = array[n-1-i];
+        array[n-1] = temp;
+    }
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    var n = array.length;
+    while (n > 1) {
+        if (array[0] > array[n]) {
+            let maxValue = 0;
+            let maxIndex = 0;
+            for(let i = 0; i < n; i++){
+                if (array[i] > maxValue) {
+                    maxValue = array[i];
+                    maxIndex = i;
+                }
+            }
+            flip(array,maxIndex);
+            flip(array,n);
+        }
+        else {
+            n = n - 1;
+        }
+    }
     return array;
 }
