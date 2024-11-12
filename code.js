4619f8eb39396f76da1ba0541afae688aa3ade17
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-12 10:43:14 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 function flip(array, n) {
     var temp  = 0;
+    // goes until half of n, allowing for even and odd lists to both be fully reversed.
     for (let i = 0; i < Math.floor(n / 2); i++) {
+        // swaps two values using temp as an intermediary.
         temp = array[i];
         array[i] = array[n-1-i];
         array[n-1-i] = temp;
@@ -11,6 +13,7 @@ function flip(array, n) {
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
     var n = array.length;
+    // for every value of the function, finds the max value of the sublist, flips it to the top of the list, then the bottom. Makes the whole thing flip flop.
     while (n > 1) {
         let maxValue = array[0];
         let maxIndex = 0;
